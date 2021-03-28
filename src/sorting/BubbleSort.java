367788e4971e96890626655441d1d72d729b8c48
Khushdeep Singh <dream.chamcore@gmail.com>
2021-03-28 09:00:25 +0530

    BubbleSort algorithm updated

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -6,15 +6,25 @@ public class BubbleSort {
 
     private static void bubbleSort(int[] arr){
         int temp;
+       // Instant startTime = Instant.now();
+        boolean isSwapped;
         for (int i = 0; i < arr.length; i++) {
+            isSwapped=false;
             for (int j = 0; j < arr.length-i-1; j++) {
                 if (arr[j]>arr[j+1]){
                     temp = arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1] = temp;
+                    isSwapped=true;
                 }
             }
+            if (!isSwapped) {
+                break;
+            }
         }
+//        Instant endTime = Instant.now();
+//        Duration interval = Duration.between(startTime,endTime);
+//        System.out.println("Time in  seconds : "+interval.getNano());
     }
 
     public static void main(String[] args) {
